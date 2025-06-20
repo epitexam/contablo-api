eaff981d26940aac15f64b24186cc41b6923546c
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-06-20 22:58:58 +0200

    Refactor AuthModule to include JwtModule for JWT configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: process.env.SECRET || '1209600s' },
+    }),
+  ],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 
 export class AuthModule { }

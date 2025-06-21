05901f95322910ce5513c90c375f3a703fdca423
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-06-22 00:01:50 +0200

    Add TypeOrmModule and UsersModule imports to ArticlesModule

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from './entities/article.entity';
 
 @Module({
+  imports: [UsersModule, TypeOrmModule.forFeature([Article])],
   controllers: [ArticlesController],
   providers: [ArticlesService],
 })
-export class ArticlesModule {}
+export class ArticlesModule { }

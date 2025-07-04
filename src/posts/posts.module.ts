1917d97199f4c443ab7f51429f2c078260b329fc
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-07-04 02:54:38 +0200

    Editing post module file

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { UsersModule } from 'src/users/users.module';
+import { ArticlesModule } from 'src/articles/articles.module';
 
 @Module({
+  imports: [UsersModule, ArticlesModule,TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
+  exports:[PostsModule]
 })
-export class PostsModule {}
+export class PostsModule { }

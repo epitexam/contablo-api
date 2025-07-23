4b0b50d8364ced2f54982612c8473093dfc8b380
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-07-23 08:08:45 +0200

    Editing user service file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,133 +6,103 @@ import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { SearchUserDto } from './dto/search-user.dto';
 import { ListUserDto } from './dto/list-user.dto';
-
+import { ProfilePublicDto } from './dto/profile.public.user.dto';
+import { UsersUtils } from './users.utils';
+import { ProfilePrivateDto } from './dto/profile.private.user.dto';
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) { }
+
+  private async findUserOrFail(uuid: string) {
+    const user = await this.usersRepository.findOne({ where: { uuid } });
+    if (!user) {
+      throw new NotFoundException("User not found.");
+    }
+    return user;
+  }
 
   async create(createUserDto: CreateUserDto) {
-    const { email, username } = createUserDto;
+    await UsersUtils.checkUserExists(this.usersRepository, 'email', createUserDto.email);
+    await UsersUtils.checkUserExists(this.usersRepository, 'username', createUserDto.username);
 
-    const existingEmail = await this.findOneByEmail(email);
-    if (existingEmail) { throw new ConflictException("Email already in use") }
-
-    const existingUsername = await this.findOneByUsername(username);
-    if (existingUsername) { throw new ConflictException("Username already in use") }
-
-    return this.usersRepository.save(this.usersRepository.create(createUserDto))
+    const user = this.usersRepository.create({ ...createUserDto });
+    return this.usersRepository.save(user);
   }
 
-  findAll() {
-    return this.usersRepository.find()
-  }
-
-  async search({ firstName, lastName, username, page = 1, pageSize = 10 }: SearchUserDto) {
+  async search(searchDto: SearchUserDto) {
     const query = this.usersRepository.createQueryBuilder('user');
 
-    if (firstName) {
-      query.andWhere('user.firstName % :firstName', { firstName });
+    if (searchDto.uuid) {
+      query.andWhere('user.uuid = :uuid', { uuid: searchDto.uuid });
     }
 
-    if (lastName) {
-      query.andWhere('user.lastName % :lastName', { lastName });
+    if (searchDto.username) {
+      query.andWhere('user.username ILIKE :username', { username: `%${searchDto.username}%` });
     }
 
-    if (username) {
-      query.andWhere('user.username % :username', { username });
+    if (searchDto.firstName) {
+      query.andWhere('user.firstName ILIKE :firstName', { firstName: `%${searchDto.firstName}%` });
     }
 
-    query.skip((page - 1) * pageSize).take(pageSize);
+    if (searchDto.lastName) {
+      query.andWhere('user.lastName ILIKE :lastName', { lastName: `%${searchDto.lastName}%` });
+    }
 
-    const [users, total] = await query.getManyAndCount();
-    const formattedUsers = users.map(user => new ListUserDto(user));
+    query.andWhere('user.private = :private', { private: false });
 
-    return { users: formattedUsers, pagination: { page, pageSize, total } };
+    const page = searchDto.page ?? 1;
+    const limit = searchDto.limit ?? 10;
+    const skip = (page - 1) * limit;
+
+    const [users, total] = await query.skip(skip).take(limit).getManyAndCount();
+    const formatted = users.map((user) => new ListUserDto(user));
+
+    return { users: formatted, total, page, take: limit };
   }
 
-  findOne(id: number) {
-    return this.usersRepository.findOne({ where: { id } })
+  async findProfileByUuid(uuid: string, userUuid?: string, userRoles?: string[]) {
+    const userInfo = await this.findUserOrFail(uuid);
+
+    if (!userInfo.isActive) {
+      throw new ForbiddenException("This user has deactivated their account.");
+    }
+
+    if (userUuid && userRoles) {
+      await UsersUtils.validateUserAccess(userInfo, userUuid, userRoles);
+      return new ProfilePrivateDto(userInfo);
+    }
+
+    if (userInfo.private) {
+      throw new ForbiddenException("This profile is private.");
+    }
+
+    return new ProfilePublicDto(userInfo);
   }
 
-  findOneByEmail(email: string) {
-    return this.usersRepository.findOne({ where: { email } })
+  async findOneByUuid(uuid: string) {
+    return this.findUserOrFail(uuid)
   }
 
   findUserTokenInfo(email: string) {
     return this.usersRepository.findOne({ where: { email }, relations: ['roles'] })
   }
 
-  findOneByUsername(username: string) {
-    return this.usersRepository.findOne({ where: { username } })
+  async update(userUuid: string, userRoles: string[], updateUserDto: UpdateUserDto) {
+    const userInfo = await this.findUserOrFail(userUuid);
+    await UsersUtils.validateUserAccess(userInfo, userUuid, userRoles);
+
+    Object.assign(userInfo, updateUserDto);
+    return new ProfilePublicDto(await this.usersRepository.save(userInfo));
   }
 
-  async findOneByUuid(uuid: string) {
-    const user = await this.usersRepository.findOne({ where: { uuid } })
+  async remove(uuid: string, userUuid: string, userRoles: string[]) {
+    const userInfo = await this.findUserOrFail(uuid)
+    await UsersUtils.validateUserAccess(userInfo, userUuid, userRoles);
 
-    if (!user) {
-      throw new NotFoundException("User not foud")
-    }
-
-    return user
-  }
-
-  async findProfileByUuid(uuid: string) {
-    const user = await this.usersRepository.createQueryBuilder('user')
-      .leftJoinAndSelect('user.articles', 'article')
-      .select([
-        'user.uuid',
-        'user.username',
-        'user.email',
-        'user.firstName',
-        'user.lastName',
-        'user.bio',
-        'user.avatarUrl',
-        'user.createdAt',
-        'user.updatedAt',
-        'article.uuid',
-        'article.title',
-        'article.slug',
-        'article.published',
-        'article.createdAt',
-        'article.tags'
-      ])
-      .where('user.uuid = :uuid', { uuid })
-      .getOne();
-
-    if (!user) return null;
-
-    const articles = (user.articles || []).map(article => ({
-      uuid: article.uuid,
-      title: article.title,
-      slug: article.slug,
-      published: article.published,
-      createdAt: article.createdAt,
-      authorUsername: user.username,
-      tags: article.tags,
-    }));
-
-    const profile = {
-      uuid: user.uuid,
-      username: user.username,
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      bio: user.bio,
-      avatarUrl: user.avatarUrl,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-      articles,
-    };
-    return profile;
-  }
-
-  update(uuid: string, updateUserDto: UpdateUserDto) {
-    return this.usersRepository.update({ uuid }, updateUserDto)
-  }
-
-  remove(uuid: string) {
-    return this.usersRepository.delete({ uuid })
+    return new ProfilePublicDto(await this.usersRepository.remove(userInfo))
   }
 }

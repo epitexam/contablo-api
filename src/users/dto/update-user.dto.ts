984d3aff87c7a677305a200b82dfc20ab174e5a2
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-07-23 08:01:13 +0200

    More documentation for private field in update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
 
@@ -51,6 +52,7 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
         example: false,
         required: true,
     })
+    @IsBoolean()
     private?: boolean;
 
     @ApiProperty({

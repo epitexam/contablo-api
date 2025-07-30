bb459f9ac9685504993a0d3633015812bc1f294f
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-07-30 16:39:32 +0200

    Enabling cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Contablo API')
     .setDescription('API to manage users, posts, and comments in Contablo forum.')

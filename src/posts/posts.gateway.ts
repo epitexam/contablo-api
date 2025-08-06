924227ae49efd826f798ad4440da44f3e35c874b
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-08-06 13:35:44 +0200

    Editing post gateway file

--- src/posts/posts.gateway.ts
+++ src/posts/posts.gateway.ts
@@ -1,9 +1,39 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { PostDto } from './dto/post-response.dto';
 
 @WebSocketGateway()
 export class PostsGateway {
+
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
+  }
+
+  @SubscribeMessage('joinArticle')
+  handleJoinArticle(@MessageBody() articleUuid: string, @ConnectedSocket() client: Socket) {
+    const roomName = `article_${articleUuid}`;
+    client.join(roomName);
+  }
+
+  handleConnection(client: Socket) {
+    const articleUuid = client.handshake.query.articleUuid;
+    if (articleUuid) {
+      const roomName = `article_${articleUuid}`;
+      client.join(roomName);
+    }
+  }
+
+  emitNewComment(articleUuid: string, comment: PostDto) {
+    const roomName = `article_${articleUuid}`;
+    this.server.to(roomName).emit('newComment', comment);
+  }
+
+  emitUpdatedComment(articleUuid: string, updatedComment: PostDto) {
+    const roomName = `article_${articleUuid}`;
+    this.server.to(roomName).emit('commentUpdated', updatedComment);
   }
 }
